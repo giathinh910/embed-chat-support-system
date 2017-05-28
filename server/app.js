9fad4ecb7d48a6e4b4f006d78ffa0d8ee591ed47
Thinh Bui <giathinh910@gmail.com>
2017-05-28 10:38:11 +0700

    attach socket.io

--- server/app.js
+++ server/app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var io = require('socket.io')();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.io = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
