d1397e6bf4e8f7e3cd2247926f2bd8e11fb4d083
Thinh Bui <giathinh910@gmail.com>
2017-06-05 17:41:54 +0700

    init angular agent app

---
+++ angular-agent/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'agent-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'agent works!';
+}
